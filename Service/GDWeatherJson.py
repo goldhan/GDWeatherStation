aff2e6f45272224024c030e6d082d3d88d5d522a
韩金 <jin.han@inin88.com>
2018-11-06 16:39:40 +0800

    Feat: 优化 Python端 代码

--- Service/GDWeatherJson.py
+++ Service/GDWeatherJson.py
@@ -33,60 +33,49 @@ class UploadWithGit(object):
 
 class GDWeatherJson(object):
 
-    heWeatherKey = ''
-    city = ''
-    yeelinkKey = ''
     heWeatherData = ''
     qiuShiData = ''
-    heWeatherYeelinkAPI = ''
     qiuShiAPI = ''
-
-    qiushiYeelinkAPI = ''
-    pushYeelinkAPI = ''
-    bitMapYeelinkAPI1 = ''
-
-    title = ''
-    detail = ''
-
     context = ssl._create_unverified_context()
 
     def __init__(self):
+        self.userJson = UserJSON()
+        self.heWeatherKey = self.userJson.heWeatherKey
+        self.city = self.userJson.city
+        self.gitUrl = self.userJson.gitUrl
+        self.title = self.userJson.title
+        self.detail = self.userJson.detail
         self.jsonParse = GDParseJson.GDJson()
         self.time = datetime.datetime
-        self.userJson = UserJSON()
-        gitUrl = 'https://github.com/goldhan/MockJSON.git'
-        self.upGit = UploadWithGit(filePath,gitUrl)
+        self.upGit = UploadWithGit(filePath,self.gitUrl)
 
     def getWeather(self):
         self.heWeatherAPI = 'https://free-api.heweather.com/v5/weather?city=' + self.city + '&key=' + self.heWeatherKey + '&lang=EN'
         with request.urlopen(self.heWeatherAPI,context=self.context) as f:
             data = f.read()
             self.heWeatherData = data.decode('utf-8')
-            self.data2yeelikJson()
+            self.data2Json()
 
     def uploadWeather(self):
+        # pass
+        print('正在push到：',self.gitUrl)
         self.upGit.upload(self.heWeatherDic)
-        # yeelinkReq = request.Request(self.heWeatherYeelinkAPI)
-        # yeelinkReq.add_header('U-ApiKey', self.yeelinkKey)
-        # yeelinkReq.add_header('content-type', 'application/json')
-        # with request.urlopen(yeelinkReq,data= bytes(self.heWeatherDic,'utf8')) as f:
-        #     self.uploadSuccess(f, ' HeWeather')
 
-    def getQiuShi(self):
-        qiuShiReq = request.Request(self.qiuShiAPI)
-        qiuShiReq.add_header('Content-Type','application/x-www-form-urlencoded')
-        qiuShiReq.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
-        with request.urlopen(qiuShiReq) as f:
-            data = f.read()
-            self.qiuShiData = data.decode('utf-8')
-            self.data2QiuShiJson()
+    # def getQiuShi(self):
+    #     qiuShiReq = request.Request(self.qiuShiAPI)
+    #     qiuShiReq.add_header('Content-Type','application/x-www-form-urlencoded')
+    #     qiuShiReq.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
+    #     with request.urlopen(qiuShiReq) as f:
+    #         data = f.read()
+    #         self.qiuShiData = data.decode('utf-8')
+    #         self.data2QiuShiJson()
 
-    def uploadQiuShi(self):
-        yeelinkReq = request.Request(self.qiushiYeelinkAPI)
-        yeelinkReq.add_header('U-ApiKey', self.yeelinkKey)
-        yeelinkReq.add_header('content-type', 'application/json')
-        with request.urlopen(yeelinkReq, data=bytes(self.qiuShiDic, 'utf8')) as f:
-            self.uploadSuccess(f,' Qiushi')
+    # def uploadQiuShi(self):
+    #     yeelinkReq = request.Request(self.qiushiYeelinkAPI)
+    #     yeelinkReq.add_header('U-ApiKey', self.yeelinkKey)
+    #     yeelinkReq.add_header('content-type', 'application/json')
+    #     with request.urlopen(yeelinkReq, data=bytes(self.qiuShiDic, 'utf8')) as f:
+    #         self.uploadSuccess(f,' Qiushi')
 
 
     # def uploadPush(self):
@@ -99,13 +88,11 @@ class GDWeatherJson(object):
     #     with request.urlopen(yeelinkReq, data=bytes(self.pushJson, 'utf8')) as f:
     #         self.uploadSuccess(f, ' Push')
 
-    def data2yeelikJson(self):
+    def data2Json(self):
         self.heWeatherDic = self.jsonParse.parseHeWeather(self.heWeatherData)
-        self.userJson.LoadPush()
-
-        self.heWeatherDic['value']['title'] = self.userJson.title
+        self.heWeatherDic['value']['title'] = self.title
         str16 = ''
-        for i, s in enumerate(self.userJson.detail):
+        for i, s in enumerate(self.detail):
             str16 += s
             if (i + 1) % 16 == 0:
                 str16 += '*+'
@@ -117,12 +104,13 @@ class GDWeatherJson(object):
 
         self.heWeatherDic['value']['detail'] = strArr
         self.heWeatherDic = json.dumps(self.heWeatherDic)
+        print(self.heWeatherDic)
 
-    def data2QiuShiJson(self):
-        self.qiuShiDic = self.jsonParse.parseQiushi(self.qiuShiData)
-        self.qiuShiDic['key'] = self.yeelinkKey
-        self.qiuShiDic = json.dumps(self.qiuShiDic)
-        # print(self.qiuShiDic)
+    # def data2QiuShiJson(self):
+    #     self.qiuShiDic = self.jsonParse.parseQiushi(self.qiuShiData)
+    #     self.qiuShiDic['key'] = self.yeelinkKey
+    #     self.qiuShiDic = json.dumps(self.qiuShiDic)
+    #     # print(self.qiuShiDic)
 
     def uploadSuccess(self,data,message):
         if data.status == 200:
--- Service/Main.py
+++ Service/Main.py
@@ -1,37 +1,9 @@
 # coding=utf-8
-
-from GDWeatherJson import GDWeatherJson
-import time,sys
-from GDUserData import UserJSON
-
-userJson = UserJSON()
-userJson.Load()
-
-
-weather = GDWeatherJson()
-weather.city = userJson.city
-weather.heWeatherKey = userJson.heWeatherKey
-weather.qiuShiAPI = userJson.qiuShiAPI
-weather.heWeatherYeelinkAPI = userJson.heWeatherYeelinkAPI
-weather.yeelinkKey = userJson.yeelinkKey
-weather.qiushiYeelinkAPI = userJson.qiushiYeelinkAPI
-weather.pushYeelinkAPI = userJson.pushYeelinkAPI
-weather.bitMapYeelinkAPI1 = userJson.bitMapYeelinkAPI1
-weather.title = userJson.title
-weather.detail = userJson.detail
-
-
-def start():
-
-    # weather.uploadPush()
-
+import time,sys,GDWeatherJson
+weather = GDWeatherJson.GDWeatherJson()
+def getAndUpload():
     weather.getWeather()
     weather.uploadWeather()
-
-#     weather.getQiuShi() 
-#     weather.uploadQiuShi()
-
-
 
 def waitTime(sec):
     for i in range(sec):
@@ -41,12 +13,12 @@ def waitTime(sec):
         sys.stdout.write('\r')
         sys.stdout.flush()
 
-while True:
-    start()
-    waitTime(10)
-    sys.stdout.write('\r')
-    sys.stdout.flush()
+def start():
+        while True:
+                getAndUpload()
+                waitTime(10)
+                sys.stdout.write('\r')
+                sys.stdout.flush()
 
-
-
+start()
 
